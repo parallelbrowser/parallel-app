ed3ee7d00c903f5a57b1f047254ec0045b9f4d1e
twillkens <tom.willkens@gmail.com>
2017-10-05 18:34:10 -0400

    friends implemented

--- js/com/header.js
+++ js/com/header.js
@@ -1,6 +1,6 @@
 const html = require('choo/html')
 const renderAvatar = require('./avatar')
-const {getViewProfileURL, getEditProfileURL, getViewWorkbenchURL, getViewShopURL, getViewSubgizmosURL} = require('../util')
+const {getEditProfileURL, getViewWorkbenchURL, getViewShopURL, getViewSubgizmosURL, getViewFindFriendURL, getViewKeysURL} = require('../util')
 
 module.exports = function renderFeed (state, emit) {
   return html`
@@ -16,6 +16,12 @@ module.exports = function renderFeed (state, emit) {
         ${state.userProfile ? html`
           <div class="avatar-card dropdown-menu-container" onclick=${onToggleDropdown}>
             <div class="dropdown-menu ${state.isDropdownOpen ? '' : 'hidden'}">
+              <a href=${getViewFindFriendURL(state.userProfile)} class="dropdown-menu-item">
+                Find a Friend
+              </a>
+              <a href=${getViewKeysURL(state.userProfile)} class="dropdown-menu-item">
+                Setup
+              </a>
               <a href=${getEditProfileURL(state.userProfile)} class="dropdown-menu-item">
                 Edit profile
               </a>
@@ -28,7 +34,7 @@ module.exports = function renderFeed (state, emit) {
               <a href=${getViewShopURL(state.userProfile)} class="dropdown-menu-item">
                 Shop
               </a>
-              <a href="https://github.com/beakerbrowser/dat-nexus-app/issues" class="dropdown-menu-item">
+              <a href="https://github.com/parallel/parallel-app/issues" class="dropdown-menu-item">
                 Report an issue
               </a>
             </div>
